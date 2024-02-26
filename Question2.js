f4dac4bea48453c2639d299731c5a48450765c42
Antonio Gleyser <antoniogleyser2@hotmail.com>
2024-02-26 14:25:52 -0300

    request url, errors

--- Question2.js
+++ Question2.js
@@ -1,15 +1,28 @@
 //Simple HTTP server
-//initiate module
-const http = require('http'); 
-const port = 3000;
+//import module
+const http = require('http');
+const PORT = 3000;
 
 //Create server
 const server = http.createServer(function (req, res) {
+    // Check if the request URL is '/'
     if (req.url === '/') {
-        res.write('Hello There!');
-        res.end();
+        res.write('<h1>Hello There!</h1>')
     }
+    // Check if the request URL is '/about'
+    if (req.url === '/about') {
+        res.write('About Page')
+    }
+    else {
+        //route 404 not found
+        res.write('404 Not Found')
+    }
+    res.end();
 
 });
-server.listen(3000);
-console.log('Listening on port 3000...')
+
+
+//start the server and listen on the specified port
+server.listen(PORT, () => {
+    console.log('Listening on port 3000...')
+});
